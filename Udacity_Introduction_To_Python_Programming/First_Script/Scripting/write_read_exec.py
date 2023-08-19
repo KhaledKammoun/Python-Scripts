578d4c851ca50688c7829e27c4a2187b52d55732
KhaledKammoun <khaledsfax03@gmail.com>
2023-08-19 21:03:01 +0100

    Udacity

--- Udacity_Introduction_To_Python_Programming/First_Script/Scripting/write_read_exec.py
+++ Udacity_Introduction_To_Python_Programming/First_Script/Scripting/write_read_exec.py
@@ -9,3 +9,12 @@ for i in range(10) :
 with open('new_file.py', 'r') as f:
     code_to_execute = f.read()
 exec(code_to_execute)
+
+"""
+REMARQUE :
+with open('all_lines.txt','w') as f:
+    f.write("Hello World!!!\nABC!!!")
+with open('all_lines.txt','r') as f :
+    x = f.readlines() 
+print(x) # ["Hello World!!!\n", "ABC!!!"]
+"""
