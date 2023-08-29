0db5835ef57fe66afc1e2c9ccadb8bc3dfb6732f
KhaledKammoun <khaledsfax03@gmail.com>
2023-08-29 15:07:01 +0100

    Advanced_Topics

--- Udacity_Introduction_To_Python_Programming/First_Script/Advanced Topics/chunker.py
+++ Udacity_Introduction_To_Python_Programming/First_Script/Advanced Topics/chunker.py
@@ -4,3 +4,8 @@ def chunker(iterable, size) :
 
 for chunk in chunker(range(25), 4):
     print(list(chunk))
+
+##### Generators and list comprehension
+sq_list = [x**2 for x in range(10)]  # this produces a list of squares
+
+sq_iterator = (x**2 for x in range(10))  # this produces an iterator of squares
