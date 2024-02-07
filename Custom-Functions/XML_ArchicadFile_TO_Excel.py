70fa63bc7185c070e1ed5269b6eaea177830e513
KhaledKammoun <khaledsfax03@gmail.com>
2024-02-08 00:45:29 +0100

    Xml To Excel Function - Modification

--- Custom-Functions/XML_ArchicadFile_TO_Excel.py
+++ Custom-Functions/XML_ArchicadFile_TO_Excel.py
@@ -1,43 +1,54 @@
 from openpyxl import Workbook
 import lxml.etree as etree
 
-# XML File
-tree = etree.parse("classification.xml")
-xpath_expression = "/BuildingInformation/Classification/System/Items/Item"
-items = tree.xpath(xpath_expression)
-
-
-# Create new Excel File
-wb = Workbook()
-ws = wb.active
-ws.append(["ID", "Name", "Description", "Niveau"])
 
 def return_row(item, niveau) :
-    id_value = item.find("ID").text
+    if (len(item.find("ID").text) == 8) or niveau == 3 :
+        id_value = item.find("ID").text
+    elif niveau == 1:
+        if len(item.find("ID").text) == 2 :
+            id_value = "{}|00|00".format(item.find("ID").text)
+        elif len(item.find("ID").text) == 1 :
+            id_value = "0{}|00|00".format(item.find("ID").text)
+    elif niveau == 2 or len(item.find("ID").text) == 5:
+        id_value = "{}|00".format(item.find("ID").text)
 
-    name_value = item.find("Name").text if not item.find("Name") else ""
+    name_value = item.find("Name").text if (not item.find("Name")) else ""
 
     description = item.find("Description").text
 
     return [id_value, name_value, description, niveau]
+def convert_xml_to_excel(input_file, output = "", output_file_name = "new_file") :
+    # XML File
+    tree = etree.parse(input_file)
+    
+    xpath_expression = "/BuildingInformation/Classification/System/Items/Item"
+    items = tree.xpath(xpath_expression)
 
-for item in items :
-    ws.append(return_row(item, 1))
-    children = item.find("Children")
-    if children is not None:
-        for item_child_1 in children.findall("Item") :
-            ws.append(return_row(item_child_1, 2))
-            children_1 = item_child_1.find("Children")
-            if children_1 is not None:
-                for item_child_2 in children_1.findall("Item") :
-                    ws.append(return_row(item_child_2, 3))
-                    children_2 = item_child_2.find("Children")
-                    if children_2 is not None:
-                        for item_child_3 in children_2.findall("Item") :
-                            ws.append(return_row(item_child_3, 4))
-                            children_3 = item_child_3.find("Children")
-                            if children_3 is not None:
-                                for item_child_4 in children_3.findall("Item") :
-                                    ws.append(return_row(item_child_4, 5))
+    # Create new Excel File
+    wb = Workbook()
+    ws = wb.active
+    ws.append(["ID", "Name", "Description", "Niveau"])
 
-wb.save("Classification.xlsx")
+    for item in items :
+        ws.append(return_row(item, 1))
+        children = item.find("Children")
+        if children is not None:
+            for item_child_1 in children.findall("Item") :
+                ws.append(return_row(item_child_1, 2))
+                children_1 = item_child_1.find("Children")
+                if children_1 is not None:
+                    for item_child_2 in children_1.findall("Item") :
+                        ws.append(return_row(item_child_2, 3))
+                        children_2 = item_child_2.find("Children")
+                        if children_2 is not None:
+                            for item_child_3 in children_2.findall("Item") :
+                                ws.append(return_row(item_child_3, 4))
+                                children_3 = item_child_3.find("Children")
+                                if children_3 is not None:
+                                    for item_child_4 in children_3.findall("Item") :
+                                        ws.append(return_row(item_child_4, 5))
+    if (len(output_file_name) >= 5 and output_file_name[-5:] != ".xlsx") :
+        wb.save("{}.xlsx".format(output_file_name))
+    else :
+        wb.save(output_file_name)
