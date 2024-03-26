aae4353c70e392385aefcc56ae0a46f6eb00632f
KhaledKammoun <khaledsfax03@gmail.com>
2024-03-26 15:32:12 +0100

    Fixing Insert Descriptif to the data base

--- mySQL/insertValues.py
+++ mySQL/insertValues.py
@@ -36,73 +36,73 @@ sql_getArticleID = "select articleID from articles where lotArticleID = %s"
 sql_select = "select * from articles"
 
 i = 1
-# try:
-#     for element in elements :
-#         id, name, descriptif, level = element
-#         # chapter
+try:
+    for element in elements :
+        id, name, descriptif, level = element
+        # chapter
 
-#         if len(id) == 2 :
-#             # add chapter to the database without a parent id
-#             values = (id+"|00|00", 1, name, None, 1)
-#             # Execute the SQL query
-#             # cursor.execute(sql, values)
-#             # print("***1 : ", values)
-#             # print("{} : Data inserted successfully!".format(i))
-#         else :
-#             id1, id2, id3 = id.split('|')
-#             # if the second id is 0, add descriptif to the chapter not the article
-#             if (not int(id2) and descriptif) :
-#                 # get articleID
-#                 cursor.execute(sql_getArticleID, (id,))
-#                 result = cursor.fetchone()  # Fetch the first row
-#                 # print("ID : ", id)
-#                 values_descriptif = (result[0], descriptif)
-#                 # cursor.execute(sql_descriptif, values_descriptif)
+        if len(id) == 2 :
+            # add chapter to the database without a parent id
+            values = (id+"|00|00", 1, name, None, 1)
+            # Execute the SQL query
+            # cursor.execute(sql, values)
+            # print("***1 : ", values)
+            # print("{} : Data inserted successfully!".format(i))
+        else :
+            id1, id2, id3 = id.split('|')
+            # if the second id is 0, add descriptif to the chapter not the article
+            if (not int(id2) and descriptif) :
+                # get articleID
+                cursor.execute(sql_getArticleID, (id,))
+                result = cursor.fetchone()  # Fetch the first row
+                # print("ID : ", id)
+                values_descriptif = (result[0], descriptif)
+                cursor.execute(sql_descriptif, values_descriptif)
 
-#             elif not int(id3) :
-#                 # if the third id is 0, add article to the data base with descriptif if there .
-#                 id_parent = (id1 + "|00|00")
-#                 sql = "INSERT INTO articles (lotArticleID, lotID, articleContent, parentArticle, userID) VALUES (%s, %s, %s, %s, %s)"
-#                 cursor.execute(sql_getArticleID, (id_parent,))
-#                 parentArticleID = cursor.fetchone()  # Fetch the first row
-#                 values = (id, 1, name, parentArticleID[0], 1)
-#                 # cursor.execute(sql, values)
+            elif not int(id3) :
+                # if the third id is 0, add article to the data base with descriptif if there .
+                # id_parent = (id1 + "|00|00")
+                # sql = "INSERT INTO articles (lotArticleID, lotID, articleContent, parentArticle, userID) VALUES (%s, %s, %s, %s, %s)"
+                # cursor.execute(sql_getArticleID, (id_parent,))
+                # parentArticleID = cursor.fetchone()  # Fetch the first row
+                # values = (id, 1, name, parentArticleID[0], 1)
+                # cursor.execute(sql, values)
 
-#                 if descriptif :
-#                     # print(values)
-#                     # get articleID
-#                     cursor.execute(sql_getArticleID, (id,))
-#                     result = cursor.fetchone()  # Fetch the first row
-#                     # print("ID : ", result[0])
-#                     values_descriptif = (result[0], descriptif)
-#                     # cursor.execute(sql_descriptif, values_descriptif)
-#             else :
-#                 # if the third id is not 0, add article to the data base with descriptif if there .
+                if descriptif :
+                    # print(values)
+                    # get articleID
+                    cursor.execute(sql_getArticleID, (id,))
+                    result = cursor.fetchone()  # Fetch the first row
+                    # print("ID : ", result[0])
+                    values_descriptif = (result[0], descriptif)
+                    cursor.execute(sql_descriptif, values_descriptif)
+            else :
+                # if the third id is not 0, add article to the data base with descriptif if there .
                 
-#                 id_parent = (id1 + "|"+id2+"|00")
-#                 sql = "INSERT INTO articles (lotArticleID, lotID, articleContent, parentArticle, userID) VALUES (%s, %s, %s, %s, %s)"
-#                 cursor.execute(sql_getArticleID, (id_parent,))
-#                 parentArticleID = cursor.fetchone()  # Fetch the first row
-#                 values = (id, 1, name, parentArticleID[0], 1)
-#                 # print(values)
-#                 # cursor.execute(sql, values)
-#                 if descriptif :
-#                     # print(values)
+                # id_parent = (id1 + "|"+id2+"|00")
+                # sql = "INSERT INTO articles (lotArticleID, lotID, articleContent, parentArticle, userID) VALUES (%s, %s, %s, %s, %s)"
+                # cursor.execute(sql_getArticleID, (id_parent,))
+                # parentArticleID = cursor.fetchone()  # Fetch the first row
+                # values = (id, 1, name, parentArticleID[0], 1)
+                # print(values)
+                # cursor.execute(sql, values)
+                if descriptif :
+                    # print(values)
                     
-#                     # get articleID
-#                     cursor.execute(sql_getArticleID, (id,))
-#                     result = cursor.fetchone()  # Fetch the first row
-#                     # print("ID : ", result[0])
-#                     values_descriptif = (result[0], descriptif)
-#                     # cursor.execute(sql_descriptif, values_descriptif)
-#         i+=1
-#     # Commit Changes
-#     connection.commit()
+                    # get articleID
+                    cursor.execute(sql_getArticleID, (id,))
+                    result = cursor.fetchone()  # Fetch the first row
+                    # print("ID : ", result[0])
+                    values_descriptif = (result[0], descriptif)
+                    cursor.execute(sql_descriptif, values_descriptif)
+        i+=1
+    # Commit Changes
+    connection.commit()
     
-# except pymysql.Error as error:
-#     # Rollback in case of an error
-#     print("Error inserting data:", error)
-#     connection.rollback()
+except pymysql.Error as error:
+    # Rollback in case of an error
+    print("Error inserting data:", error)
+    connection.rollback()
 
 
 # test the data base :
